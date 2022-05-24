7619ee18f92101bc0304027bd08ec4c550e67022
userao <ostapenko2121@gmail.com>
2022-05-24 18:08:35 +0700

    gendiff refractored, README appended

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import * as path from 'node:path';
 
 const dataToText = (code) => {
   const dataArray = JSON.parse(code).data;
@@ -7,10 +9,15 @@ const dataToText = (code) => {
 };
 
 const genDiff = (path1, path2) => {
-  const firstStringified = JSON.stringify(readFileSync(path1));
-  const secondStringified = JSON.stringify(readFileSync(path2));
-  const firstObjectFromData = JSON.parse(dataToText(firstStringified));
-  const secondObjectFromData = JSON.parse(dataToText(secondStringified));
+  const workingDir = cwd();
+  const resolvedPath1 = path.resolve(workingDir, path1);
+  const resolvedPath2 = path.resolve(workingDir, path2);
+  const codedData1 = readFileSync(resolvedPath1);
+  const codedData2 = readFileSync(resolvedPath2);
+  const firstDataString = JSON.stringify(codedData1);
+  const secondDataString = JSON.stringify(codedData2);
+  const firstObjectFromData = JSON.parse(dataToText(firstDataString));
+  const secondObjectFromData = JSON.parse(dataToText(secondDataString));
   const allKeys = _.union(Object.keys(firstObjectFromData), Object.keys(secondObjectFromData));
   const sortedKeys = _.sortBy(allKeys);
 
