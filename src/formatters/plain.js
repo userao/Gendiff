c33e0aa55c700f1661419986962cda4a40fa7ead
userao <ostapenko2121@gmail.com>
2022-06-15 21:37:31 +0700

    code refractored #12

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -16,30 +16,26 @@ const stringify = (value) => {
   }
 };
 
-const createString = (leaf, path) => {
-  const { key, state } = leaf;
-  const currentPath = `${path}${key}`;
-  if (state === 'unchanged') return [];
-  if (state === 'changed') {
-    const { from, to } = leaf;
-    return `Property '${currentPath}' was updated. From ${stringify(from)} to ${stringify(to)}`;
-  }
-  if (state === 'added') {
-    const { value } = leaf;
-    return `Property '${currentPath}' was added with value: ${stringify(value)}`;
-  }
-  return `Property '${currentPath}' was removed`;
-};
-
 const plain = (differences) => {
   if (_.isEqual(differences, {})) return '{}';
   const iter = (node, path = '') => node.flatMap((leaf) => {
-    const { key, state } = leaf;
-    if (state === 'nested') {
-      const { children } = leaf;
-      return iter(children, `${path}${key}.`);
+    const { key, state, children } = leaf;
+    const currentPath = `${path}${key}`;
+
+    switch (state) {
+      case 'nested':
+        return iter(children, `${path}${key}.`);
+      case 'changed':
+        return `Property '${currentPath}' was updated. From ${stringify(leaf.from)} to ${stringify(leaf.to)}`;
+      case 'added':
+        return `Property '${currentPath}' was added with value: ${stringify(leaf.value)}`;
+      case 'removed':
+        return `Property '${currentPath}' was removed`;
+      case 'unchanged':
+        return [];
+      default:
+        throw new Error(`Unknown state: ${state}`);
     }
-    return createString(leaf, path);
   })
     .join('\n')
     .trim();
